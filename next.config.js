a5474d05888ba974c64bc005cfecdc9c06e58c4f
jaden <yeonhos2@naver.com>
2024-12-08 14:36:39 +0900

    chore: gitpage 배포 세팅

--- next.config.js
+++ next.config.js
@@ -6,23 +6,10 @@ const { NODE_ENV } = process.env;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: (() => {
-    if (NODE_ENV === 'production' && homepage) {
-      try {
-        console.log('> Detected homepage url in package.json');
-        const { pathname } = new URL(homepage);
-        if (pathname !== '/') {
-          console.log(`> Apply \'${pathname}\' to assetPrefix(subPath)`);
-          return pathname;
-        }
-        return '';
-      } catch {
-        console.log('> Can not parse homepage URL not apply assetPrefix(subPath)');
-        return '';
-      }
-    }
-    return '';
-  })(),
+  assetPrefix:
+  process.env.NODE_ENV === "production"
+    ? "https://jadenspace.github.io/resume"
+    : "",
   webpack5: false,
   eslint: {
     ignoreDuringBuilds: true,
